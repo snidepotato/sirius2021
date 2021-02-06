38ee50b1c60f3dcc35698f9ca4f1c64d5e763619
Dmitry <dmitry@macbook.local>
2021-02-06 20:57:25 +0300

    routine

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include"DiffusionOU.h"
 #include"DiffusionCIR.h"
 #include"DiffusionLipton.h"
+#include"IRP.h"
+
 
 #include<iostream>
 
@@ -11,36 +13,9 @@ using namespace SiriusFM;
 
 int main()
 {
-		double m = 0;
-		double s = 0;
-		double b = 0;
-		double t = 0;
-		double kappa = 0; 
-		double theta = 0;
-		double s_0, s_1, s_2 = 0;
+	IRProvider<CcyE::Const> A("rates.txt");
 
-		cin >> m;
-		cin >> s;
-		cin >> b;
-		cin >> s_0 >> s_1 >> s_2 >> kappa >> theta;
-
-		DiffusionGBM A(m, s);
-		DiffusionCEV B(m, s, b);
-		DiffusionLipton C(m, s_0, s_1, s_2);
-		DiffusionOU D(kappa, theta, s);
-		DiffusionCIR E(kappa, theta, s);
-		
-		cout << "S_t\n";
-		cin >> t;
-
-		cout << A.mu(t) << " " << A.sigma(t) << endl;
-		cout << B.mu(t) << " " << B.sigma(t) << endl;
-		cout << C.mu(t) << " " << C.sigma(t) << endl;
-		cout << D.mu(t) << " " << D.sigma(t) << endl;
-		cout << E.mu(t) << " " << E.sigma(t) << endl;
-
-		return 0;
-
+	return 0;
 }
 
 		
