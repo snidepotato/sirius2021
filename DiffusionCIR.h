42e17625ecc06114f768669bc0861a0a1b5e1c0b
Dmitry <dmitry@macbook.local>
2021-02-10 21:20:00 +0300

    minor fixes in diffusions

--- DiffusionCIR.h
+++ DiffusionCIR.h
@@ -5,24 +5,24 @@ namespace SiriusFM
                 
 	class DiffusionCIR
 	{
-			double const m_theta;
-			double const m_kappa;
-			double const m_sigma;
+		double const m_theta;
+		double const m_kappa;
+		double const m_sigma;
 
-		public:
-			DiffusionCIR(double m, double s, double t): m_theta(m), 
-											  			m_kappa(s),
-														m_sigma(t)
+	public:
+		DiffusionCIR(double m, double s, double t): m_theta(m), 
+											  		m_kappa(s),
+													m_sigma(t)
+		{
+			if(m_sigma <= 0)
 			{
-					if(m_sigma <= 0)
-					{
-					}
-			};
+			}
+		};
 
-			double mu(double S_t, double t) {return m_kappa*(m_theta -  S_t);};
-			double sigma(double S_t = 0, double t) 
-			{
-				return m_sigma * sqrt(S_t);
-			};
+		double mu(double S_t, double t) {return m_kappa*(m_theta -  S_t);};
+		double sigma(double S_t = 0, double t) 
+		{
+			return m_sigma * sqrt(S_t);
+		};
 	};
 }
--- DiffusionOU.h
+++ DiffusionOU.h
@@ -6,21 +6,21 @@ namespace SiriusFM
                 
 	class DiffusionOU
 	{
-			double const m_theta;
-			double const m_kappa;
-			double const m_sigma;
+		double const m_theta;
+		double const m_kappa;
+		double const m_sigma;
 
-		public:
-			DiffusionOU(double m, double s, double t): 	m_theta(m),
-														m_kappa(s), 
-														m_sigma(t)
+	public:
+		DiffusionOU(double m, double s, double t): 	m_theta(m),
+													m_kappa(s), 
+													m_sigma(t)
+		{
+			if(m_sigma <= 0)
 			{
-					if(m_sigma <= 0)
-					{
-					}
-			};
+			}
+		};
 
-			double mu(double S_t, double t) {return m_kappa*(m_theta -  S_t);};
-			double sigma(double S_t = 0, double t) {return m_sigma;};
+		double mu(double S_t, double t) {return m_kappa*(m_theta -  S_t);};
+		double sigma(double S_t = 0, double t) {return m_sigma;};
 	};
 }
