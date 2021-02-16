a2106c8ff73af144dd83c7e3a0955b2783f0383d
Dmitry <dmitry@62.183.4.6.modem-pool.kuban.ru>
2021-02-16 12:10:58 +0300

    minor fixes in DiffusionCIR.h

--- DiffusionCIR.h
+++ DiffusionCIR.h
@@ -4,7 +4,6 @@
 
 namespace SiriusFM
 {
-                
 	class DiffusionCIR
 	{
 		double const m_theta;
@@ -13,11 +12,14 @@ namespace SiriusFM
 		double const m_S0
 
 	public:
-		DiffusionCIR(double m, double s, double t, double a_S0): 
-													m_theta(m), 
-											  		m_kappa(s),
-													m_sigma(t),
-													m_S0(a_S0)
+		DiffusionCIR(double a_theta,
+					 double a_kappa,
+					 double a_sigma,
+					 double a_S0): 
+			m_theta(a_theta), 
+			m_kappa(a_kappa),
+			m_sigma(a_sigma),
+			m_S0(a_S0)
 		{
 			if(m_sigma <= 0)
 			{
@@ -25,10 +27,10 @@ namespace SiriusFM
 			}
 		};
 
-		double mu(double S_t, double t) {return m_kappa*(m_theta -  S_t);};
-		double sigma(double S_t = 0, double t) 
+		double mu(double a_S, double t) const {return m_kappa*(m_theta - a_S);};
+		double sigma(double a_S = 0, double t) const 
 		{
-			return m_sigma * sqrt(S_t);
+			return m_sigma * sqrt(a_S);
 		};
 		double GetStartPoint() const {return m_S0;}
 	};
