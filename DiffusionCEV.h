30f2c53c1609be89218b694033721de8a17fcb19
Dmitry <dmitry@62.183.4.6.modem-pool.kuban.ru>
2021-02-16 12:14:31 +0300

    minor fixes in DiffusionCEV.h

--- DiffusionCEV.h
+++ DiffusionCEV.h
@@ -6,24 +6,30 @@ namespace SiriusFM
                 
 	class DiffusionCEV
 	{
-		double const m_muBar;
-		double const m_sigmaBar;
+		double const m_mu;
+		double const m_sigma;
 		double const m_beta;
+		double const m_S0;
 
 	public:
-		DiffusionCEV(double m, double s, double b): m_muBar(m), 
-											  		m_sigmaBar(s),
-													m_beta(b)
+		DiffusionCEV(double a_mu,
+					 double a_sigma,
+					 double a_beta,
+					 double a_S0):
+			m_mu(a_mu), 
+			m_sigma(a_sigma),
+			m_beta(a_beta),
+			m_S0(a_S0)
 		{
-			if(m_sigmaBar <= 0 || m_beta <= 0)
-			{
-			}
+			if(m_sigma <= 0 || m_beta <= 0)
+				throw std::invalid_argument("Bad diff parameters");
 		};
 
-		double mu(double S_t, double t) {return m_muBar * S_t;};
-		double sigma(double S_t, double t) 
+		double mu(double a_S, double t) const {return m_mu * a_S;};
+		double sigma(double a_S, double t) const 
 		{
-			return m_sigmaBar * pow(S_t, m_beta);
+			return m_sigma * pow(a_S, m_beta);
 		};
+		double GetStartPoint() const {return m_S0;};
 	};
 }
