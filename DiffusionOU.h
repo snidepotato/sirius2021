d6267d503170d8cf53f1bb0d038aacfacc664e9c
Dmitry <dmitry@62.183.4.6.modem-pool.kuban.ru>
2021-02-16 12:08:14 +0300

    minor fixes in DiffusionOU.h

--- DiffusionOU.h
+++ DiffusionOU.h
@@ -1,26 +1,32 @@
 #pragma once
 #include<cmath>
+#include<stexcept>
 
 namespace SiriusFM
 {
-                
 	class DiffusionOU
 	{
 		double const m_theta;
 		double const m_kappa;
 		double const m_sigma;
+		double const m_S0;
 
 	public:
-		DiffusionOU(double m, double s, double t): 	m_theta(m),
-													m_kappa(s), 
-													m_sigma(t)
+		DiffusionOU(double a_theta,
+					double a_kappa,
+					double a_sigma,
+					double a_S0):
+			m_theta(a_theta),
+			m_kappa(a_kappa), 
+			m_sigma(a_sigma),
+			m_S0(a_S0)
 		{
 			if(m_sigma <= 0)
-			{
-			}
+				throw std::invalid_argument("Bad sigma");
 		};
 
-		double mu(double S_t, double t) {return m_kappa*(m_theta -  S_t);};
-		double sigma(double S_t = 0, double t) {return m_sigma;};
+		double mu(double a_S, double t) const {return m_kappa*(m_theta-a_S);};
+		double sigma(double a_S = 0, double t) const {return m_sigma;};
+		double GetStartPoint() const {return m_S0;};
 	};
 }
